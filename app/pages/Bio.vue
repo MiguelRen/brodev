2cdcaac0d4e89fe0d1386ee3ea42ab0adab3f83b
miguelren <migueleduardorengel@gmail.com>
2025-11-07 22:19:49 -0400

    fix: resizing the whole Bio.vue file

--- app/pages/Bio.vue
+++ app/pages/Bio.vue
@@ -1,23 +1,23 @@
 <template>
-  <div class="max-w-md mx-auto h-screen flex flex-col">
-    <!-- Top panel: 45% of viewport height on mobile, full width -->
-           <div class="back-image  relative min-h-[60vh] w-full flex items-center justify-center">
+  <div class="w-screen h-screen flex flex-col">
+    <!-- A part begin -->
+           <div class="back-image relative w-full h-1/2 flex items-center justify-center">
              
-             <div class="absolute inset-0 bg-green-600/90 text-white">
+             <div class="absolute  w-full h-full inset-0 bg-green-600/90 text-white">
 
-              <div class ="relative z-10 flex flex-col items-center p-4 sm:p-8  text-2xl">
-                <img src="../assets/images/luis-face-fat.png" alt="Profile" class="md:mt-1  size-15  sm:size-25   rounded-full object-cover " />
-                <div class="sm:mt-6 text-2xl sm:text-4xl  font-bold" >Luis Rengel</div>
+              <div class ="relative z-10 flex flex-col items-center ">
+                <img src="../assets/images/luis-face-fat.png" alt="Profile" class=" mt-10 w-30 h-30 rounded-full object-cover " />
+                <div class="mt-5 text-4xl font-bold" >Luis Rengel</div>
 
-                <div class=" text-lg sm:text-2xl ">Asesor Inmobiliario</div>
+                <div class="text-2xl font-semibold ">Asesor Inmobiliario</div>
                 
-                <div class="mt-1 sm:mt-8  sm:text-2xl text-center " > 
-                  <h4 class="mb-3 text-sm font-semibold ">Cuentas Oficiales</h4>
+                <div class="mt-6 text-2xl justify-center " > 
+                  <h4 class=" font-semibold text-center ">Cuentas Oficiales</h4>
                   
-                  <div class="flex items-center space-x-8 sm:space-x-12 justify-center">
-                    <img src="../assets/images/brand-facebook.svg" alt="facebook-brand" class="filter brightness-0 invert h-7 w-7 sm:h-10 sm:w-10">
-                    <img src="../assets/images/brand-instagram.svg" alt="instagram-brand " class="filter brightness-0 invert h-7 w-7 sm:h-10 sm:w-10"></img>
-                    <img src="../assets/images/brand-tiktok.svg" alt="tiktok-brand" class="filter brightness-0 invert h-7 w-7 sm:h-10 sm:w-10"></img>
+                  <div class="flex justify-around gap-4">
+                    <img src="../assets/images/brand-facebook.svg" alt="facebook-brand" class="mt-4  size-15  filter brightness-0 invert">
+                    <img src="../assets/images/brand-instagram.svg" alt="instagram-brand " class="mt-4 size-15 filter brightness-0 invert"></img>
+                    <img src="../assets/images/brand-tiktok.svg" alt="tiktok-brand" class="mt-4 size-15 filter brightness-0 invert"></img>
 
                   </div>
 
@@ -27,29 +27,28 @@
               </div>
              </div>
            </div> 
+<!-- A part end -->
+ <!-- B part begin -->
 
-
-    <div class=" p-1 flex flex-col flex-1 justify-center gap-2">
+  <div class="h-1/2 flex flex-col items-center justify-center w-4/5 mx-auto gap-8 text-center">
       <button type="button"
-        class="w-full bg-green-500 hover:bg-green-700 text-white font-bold py-4 px-4 rounded-xl">Mensaje
+        class="w-full bg-green-500 hover:bg-green-700 text-white text-3xl font-bold py-4  px-4 rounded-xl">Mensaje
         WhatsApp</button>
       <button type="button"
-        class="w-full bg-blue-500 hover:bg-blue-700 text-white font-bold py-4 px-4 rounded-xl">Llama Ahora
+        class="w-full bg-blue-500 hover:bg-blue-700 text-white text-3xl font-bold py-4 px-4 rounded-xl">Llama Ahora
         !!!</button>
        <nuxt-link to="/" 
-        class="inline-flex item-center justify-centertransition duration-300 ease-in-out 
-           shadow-lg "
+        class=" 
+            cursor-pointer w-full bg-violet-500 hover:bg-violet-700 text-white text-3xl font-bold py-4 px-4 rounded-xl"
         >
-        <button type="button"
-        class=" cursor-pointer w-full bg-violet-500 hover:bg-violet-700 text-white font-bold py-4 px-4 rounded-xl">
-       
+        
         Plusbienes.com
-       </button> 
+   
       </nuxt-link>
        
 
-      <!-- Additional content goes here -->
     </div>
+    <!-- B part end -->
   </div>
 </template>
 
